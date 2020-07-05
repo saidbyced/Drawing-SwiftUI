5b76a60a8a5ab10c17aea25f38bd030ad00e12a2
Chris Eadie <chris.eadie@outlook.com>
2020-07-05 16:05:29 +0100

    Initial Commit

---
+++ Drawing/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Drawing
+//
+//  Created by Chris Eadie on 05/07/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Chris Eadie on 05/07/2020.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
