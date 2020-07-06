0b5d95d68d9138af4fc2eaab5c19031889bac120
Chris Eadie <chris.eadie@outlook.com>
2020-07-06 10:21:43 +0100

    Play with shapes and frames

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -8,13 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!").padding()
+  var body: some View {
+    GeometryReader { geometry in
+      ZStack {
+        Color.purple
+          .edgesIgnoringSafeArea(.all)
+        Color.white.frame(width: geometry.size.width - 5, height: geometry.size.height - 5)
+          .edgesIgnoringSafeArea(.all)
+        TriangleFromRect()
+          .stroke(Color.red, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+          .frame(width: 300, height: 300)
+        TriangleFromPath()
+      }
     }
+  }
+  
+  struct TriangleFromPath: View {
+    var body: some View {
+      Path { path in
+        path.move(to: CGPoint(x: 200/3 + 50, y: 0 + 50))
+        path.addLine(to: CGPoint(x: 400/3 + 50, y: 300/3 + 50))
+        path.addLine(to: CGPoint(x: 0 + 50, y: 300/3 + 50))
+        path.addLine(to: CGPoint(x: 200/3 + 50, y: 0 + 50))
+      }
+      .stroke(Color.blue, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+    }
+  }
+  
+  struct TriangleFromRect: Shape {
+    func path(in rect:CGRect) -> Path {
+      var path = Path()
+      
+      path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+      path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+      path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+      path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+      
+      return path
+    }
+  }
 }
 
+
+
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
